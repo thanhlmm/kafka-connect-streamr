4aee0bf840c7caeb337c0126be1487844e002e67
Thanh Le <mihthanh27@gmail.com>
2021-11-24 16:51:10 +0700

    Init prj

---
+++ my-source-connector/src/test/java/com/opencredo/examples/MySourceConnectorConfigTest.java
@@ -0,0 +1,10 @@
+package com.opencredo.examples;
+
+import org.junit.Test;
+
+public class MySourceConnectorConfigTest {
+  @Test
+  public void doc() {
+    System.out.println(MySourceConnectorConfig.conf().toRst());
+  }
+}
---
+++ my-source-connector/src/test/java/com/opencredo/examples/MySourceConnectorTest.java
@@ -0,0 +1,10 @@
+package com.opencredo.examples;
+
+import org.junit.Test;
+
+public class MySourceConnectorTest {
+  @Test
+  public void test() {
+    // Congrats on a passing test!
+  }
+}
